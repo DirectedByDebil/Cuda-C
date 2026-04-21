e19cc85185a562c57f92e4caee7aa2c9188c6b3d
SexKing <chromov-arsenij@mail.ru>
2026-04-21 21:11:01 +0300

    lab_1

---
+++ build.sh
@@ -0,0 +1,5 @@
+clear
+make -f Makefile_1 clean
+make -f Makefile_1
+#find . -name "*.c" -o -name "*.h" | xargs du -ch
+./cuda_1
---
+++ main.cu
@@ -0,0 +1,110 @@
+#include <stdio.h>
+
+#include "example/common/book.h"
+
+
+__global__ void add (int a, int b, int* c)
+{
+    *c = a + b;
+}
+
+void testAdd()
+{
+
+    int c;
+    int *dev_c;
+    HANDLE_ERROR( cudaMalloc( (void**)&dev_c, sizeof(int) ) );
+    
+    int a = 3;
+    int b = 4;
+    
+    add<<<1,1>>>(a, b, dev_c);
+    
+    HANDLE_ERROR( cudaMemcpy( &c, dev_c, sizeof(int), cudaMemcpyDeviceToHost) );
+    
+    printf("%d + %d = %d\n", a, b, c);
+    cudaFree(dev_c);
+}
+
+
+void viewDeviceProps()
+{
+    int count;
+    HANDLE_ERROR( cudaGetDeviceCount(&count) );
+    printf("Device count: %d\n", count);
+    
+    cudaDeviceProp prop;
+    for (int i = 0; i < count; i++)
+    {
+        HANDLE_ERROR( cudaGetDeviceProperties( &prop, i) );
+        printf("\n=== Device Base Info ===\n");
+        printf("Name: %s\n", prop.name);
+        printf("Major/minor: %d.%d\n", prop.major, prop.minor);
+
+        int clockRateKHz;
+        cudaDeviceGetAttribute(&clockRateKHz, cudaDevAttrClockRate, i);
+        printf("Clock rate: %d\n", clockRateKHz);
+
+        int deviceOverlap;
+        cudaDeviceGetAttribute(&deviceOverlap, cudaDevAttrGpuOverlap, i);
+        printf("Device Overlap: %d\n", deviceOverlap);
+
+
+        printf("\n=== Device Memory ===\n");
+        printf("Total Global Mem: %ld\n", prop.totalGlobalMem);
+        printf("Total Const Mem: %ld\n", prop.totalConstMem);
+        printf("Mem Pitch: %ld\n", prop.memPitch);
+        printf("Texture Alignment: %ld\n", prop.textureAlignment);
+        
+        
+        printf("\n=== Device Multi Processors ===\n");
+        printf("mpCount: %d\n", prop.multiProcessorCount);
+        printf("shared mem per block: %ld\n", prop.sharedMemPerBlock);
+        printf("regs per block: %d\n", prop.regsPerBlock);
+        printf("warp size: %d\n", prop.warpSize);
+        printf("max threads per block: %d\n", prop.maxThreadsPerBlock);
+        printf("max threads dims: (%d %d %d)\n", prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2]);
+        printf("max grid size: (%d %d %d)\n", prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]);
+        
+        
+        printf("\n=== Device Additional Info ===\n");
+        printf("Integrated: %d\n", prop.integrated);
+        printf("canMapHostMemory: %d\n", prop.canMapHostMemory);
+
+        int kernelTimeout;
+        cudaDeviceGetAttribute(&kernelTimeout, cudaDevAttrKernelExecTimeout , i);
+        printf("kernelExecTimeoutEnabled: %d\n", kernelTimeout);
+        
+        printf("\n");
+    }
+}
+
+void filterDevices()
+{
+    
+    int dev;
+    HANDLE_ERROR( cudaGetDevice(&dev) );
+    printf("Current device id: %d\n", dev);
+    
+    int count;
+    HANDLE_ERROR( cudaGetDeviceCount(&count) );
+    
+    cudaDeviceProp prop;
+    memset(&prop, 0, sizeof(cudaDeviceProp));
+    prop.major = 1;
+    prop.minor = 3;
+    
+    HANDLE_ERROR( cudaChooseDevice(&dev, &prop) );
+    printf("Filtered device id: %d\n", dev);
+
+    HANDLE_ERROR( cudaSetDevice(dev));
+}
+
+
+int main (int argc, char* argv[])
+{
+
+    viewDeviceProps();
+    
+    return 0;
+}
