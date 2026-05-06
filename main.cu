3be36769cf24008c6faddbac35f12d7421b38431
SexKing <chromov-arsenij@mail.ru>
2026-05-06 14:07:43 +0300

    completed lab 5

--- main.cu
+++ main.cu
@@ -14,6 +14,10 @@
 #define rnd( x ) (x * rand() / RAND_MAX)
 #define SPHERES 2000
 
+#define MAX_TEMP 1.0f
+#define MIN_TEMP 0.0001f
+#define SPEED 0.25f
+
 
 #pragma region Chapter 3
 
@@ -664,6 +668,387 @@ void testSpheres()
 #pragma endregion
 
 
+#pragma region Chapter 7
+
+struct DataBlock_heat 
+{
+	unsigned char	*output_bitmap;
+	float			*dev_inSrc;
+	float			*dev_outSrc;
+	float			*dev_constSrc;
+	CPUAnimBitmap	*bitmap;
+	cudaEvent_t		start, stop;
+	float			totalTime;
+	float			frames;
+
+    cudaTextureObject_t texIn;
+    cudaTextureObject_t texOut;
+    cudaTextureObject_t texConstSrc;
+    bool isTex2D;
+};
+
+__global__ void blend_kernel( float *outSrc, const float *inSrc )
+{
+	int x = threadIdx.x + blockIdx.x * blockDim.x;
+	int y = threadIdx.y + blockIdx.y * blockDim.y;
+	int offset = x + y * blockDim.x * gridDim.x;
+	
+	int left = offset - 1;
+	int right = offset + 1;
+	if(x == 0) left++;
+	if(x == DIM-1) right--;
+	
+	int top = offset - DIM;
+	int bottom = offset + DIM;
+	if(y == 0) top += DIM;
+	if(y == DIM-1) bottom -= DIM;
+	
+	outSrc[offset] = inSrc[offset] + SPEED * (inSrc[top] + inSrc[bottom] + inSrc[left] + inSrc[right] - inSrc[offset] * 4);
+}
+__global__ void blend_kernel_tex( float *dst, const cudaTextureObject_t tex )
+{
+	int x = threadIdx.x + blockIdx.x * blockDim.x;
+	int y = threadIdx.y + blockIdx.y * blockDim.y;
+	int offset = x + y * blockDim.x * gridDim.x;
+	
+	int left = offset - 1;
+	int right = offset + 1;
+	if(x == 0) left++;
+	if(x == DIM-1) right--;
+	
+	int top = offset - DIM;
+	int bottom = offset + DIM;
+	if(y == 0) top += DIM;
+	if(y == DIM-1) bottom -= DIM;
+    
+    float t, l, c, r, b;
+    t = tex1Dfetch<float>(tex, top);
+    l = tex1Dfetch<float>(tex, left);
+    c = tex1Dfetch<float>(tex, offset);
+    r = tex1Dfetch<float>(tex, right);
+    b = tex1Dfetch<float>(tex, bottom);
+    
+
+    dst[offset] = c + SPEED * (t + b + r + l - 4 * c);
+}
+__global__ void blend_kernel_tex2D( float *dst, const cudaTextureObject_t tex )
+{
+	int x = threadIdx.x + blockIdx.x * blockDim.x;
+	int y = threadIdx.y + blockIdx.y * blockDim.y;
+	int offset = x + y * blockDim.x * gridDim.x;
+	
+    float t, l, c, r, b;
+    t = tex2D<float>(tex, x, y-1);
+    l = tex2D<float>(tex, x-1, y);
+    c = tex2D<float>(tex, x, y);
+    r = tex2D<float>(tex, x+1, y);
+    b = tex2D<float>(tex, x, y+1);
+
+    dst[offset] = c + SPEED * (t + b + r + l - 4 * c);
+}
+
+__global__ void copy_const_kernel ( float *iptr, const float *cptr ) 
+{
+	int x = threadIdx.x + blockIdx.x * blockDim.x;
+	int y = threadIdx.y + blockIdx.y * blockDim.y;
+	int offset = x + y * blockDim.x * gridDim.x;
+	
+	if(cptr[offset] != 0) iptr[offset] = cptr[offset];
+}
+__global__ void copy_const_kernel_tex ( float *iptr, const cudaTextureObject_t& texConstSrc ) 
+{
+	int x = threadIdx.x + blockIdx.x * blockDim.x;
+	int y = threadIdx.y + blockIdx.y * blockDim.y;
+	int offset = x + y * blockDim.x * gridDim.x;
+	
+    float c = tex1Dfetch<float>(texConstSrc, offset);
+    if (c != 0)
+        iptr[offset] = c;
+}
+__global__ void copy_const_kernel_tex2D ( float *iptr, const cudaTextureObject_t& texConstSrc ) 
+{
+	int x = threadIdx.x + blockIdx.x * blockDim.x;
+	int y = threadIdx.y + blockIdx.y * blockDim.y;
+	int offset = x + y * blockDim.x * gridDim.x;
+	
+    float c = tex2D<float>(texConstSrc, x, y);
+    if (c != 0)
+        iptr[offset] = c;
+}
+
+void anim_gpu( DataBlock_heat *d, int ticks ) 
+{
+	HANDLE_ERROR( cudaEventRecord( d->start, 0 ) );
+	dim3 blocks(DIM/16,DIM/16);
+	dim3 threads(16,16);
+	CPUAnimBitmap *bitmap = d->bitmap;
+	
+	for(int i=0; i<90; i++)
+    {
+		copy_const_kernel<<<blocks,threads>>>(d->dev_inSrc,d->dev_constSrc);
+		blend_kernel<<<blocks,threads>>>(d->dev_outSrc,d->dev_inSrc);
+		swap( d->dev_inSrc, d->dev_outSrc );
+	}
+	float_to_color<<<blocks,threads>>>( d->output_bitmap, d->dev_inSrc );
+	
+	HANDLE_ERROR( cudaMemcpy( bitmap->get_ptr(), d->output_bitmap, bitmap->image_size(), cudaMemcpyDeviceToHost ) );
+	HANDLE_ERROR( cudaEventRecord( d->stop, 0 ) );
+	HANDLE_ERROR( cudaEventSynchronize( d->stop ) );
+	float elapsedTime;
+	HANDLE_ERROR( cudaEventElapsedTime( &elapsedTime, d->start, d->stop ) );
+	
+	d->totalTime += elapsedTime;
+	++d->frames;
+	printf("Average time per frame: %3.1f ms\n", d->totalTime/d->frames);
+}
+
+void anim_gpu_tex( DataBlock_heat *d, int ticks ) 
+{
+	HANDLE_ERROR( cudaEventRecord( d->start, 0 ) );
+	dim3 blocks(DIM/16,DIM/16);
+	dim3 threads(16,16);
+	CPUAnimBitmap *bitmap = d->bitmap;
+	
+    bool dstOut = true;
+	for(int i=0; i<90; i++)
+    {
+        float *in, *out;
+        cudaTextureObject_t tex;
+        if (dstOut)
+        {
+            in = d->dev_inSrc;
+            out = d->dev_outSrc;
+            tex = d->texIn;
+        }
+        else 
+        {
+            out = d->dev_inSrc;
+            in = d->dev_outSrc;
+            tex = d->texOut;
+        }
+
+        if (d->isTex2D)
+        {
+            copy_const_kernel_tex2D<<<blocks,threads>>>(in, d->texConstSrc);
+            blend_kernel_tex2D<<<blocks,threads>>>(out, tex);
+        }
+        else
+        {
+            copy_const_kernel_tex<<<blocks,threads>>>(in, d->texConstSrc);
+            blend_kernel_tex<<<blocks,threads>>>(out, tex);
+        }
+		dstOut = !dstOut;
+	}
+	float_to_color<<<blocks,threads>>>( d->output_bitmap, d->dev_inSrc );
+	
+	HANDLE_ERROR( cudaMemcpy( bitmap->get_ptr(), d->output_bitmap, bitmap->image_size(), cudaMemcpyDeviceToHost ) );
+	HANDLE_ERROR( cudaEventRecord( d->stop, 0 ) );
+	HANDLE_ERROR( cudaEventSynchronize( d->stop ) );
+	float elapsedTime;
+	HANDLE_ERROR( cudaEventElapsedTime( &elapsedTime, d->start, d->stop ) );
+	
+	d->totalTime += elapsedTime;
+	++d->frames;
+	printf("Average time per frame: %3.1f ms\n", d->totalTime/d->frames);
+}
+
+void anim_exit( DataBlock_heat *d ) 
+{
+    cudaDestroyTextureObject(d->texIn);
+    cudaDestroyTextureObject(d->texOut);
+    cudaDestroyTextureObject(d->texConstSrc);
+	cudaFree( d->dev_inSrc );
+	cudaFree( d->dev_outSrc );
+	cudaFree( d->dev_constSrc );
+	
+	HANDLE_ERROR( cudaEventDestroy( d->start ) );
+	HANDLE_ERROR( cudaEventDestroy( d->stop ) ); 
+}
+
+void test_heat_animation ()
+{
+	DataBlock_heat	data;
+	CPUAnimBitmap bitmap( DIM, DIM, &data );
+	data.bitmap = &bitmap;
+	data.totalTime = 0;
+	data.frames = 0;
+	HANDLE_ERROR( cudaEventCreate( &data.start ) );
+	HANDLE_ERROR( cudaEventCreate( &data.stop ) );
+	
+	HANDLE_ERROR( cudaMalloc( (void**)&data.output_bitmap, bitmap.image_size() ) );
+	
+	// assume float == 4 chars in size (ie., rgba)
+    HANDLE_ERROR( cudaMalloc( (void**)&data.dev_inSrc, bitmap.image_size() ) );
+	HANDLE_ERROR( cudaMalloc( (void**)&data.dev_outSrc, bitmap.image_size() ) );
+	HANDLE_ERROR( cudaMalloc( (void**)&data.dev_constSrc, bitmap.image_size() ) );
+    
+	float *temp = (float*)malloc( bitmap.image_size() );
+	for(int i=0; i<DIM*DIM; i++){
+		temp[i] = 0;
+		int x = i % DIM;
+		int y = i / DIM;
+		if((x>300) && (x<600) && (y>310) && (y<601))
+			temp[i] = MAX_TEMP;
+	}
+	temp[DIM*100+100] = (MAX_TEMP + MIN_TEMP)/2;
+	temp[DIM*700+100] = MIN_TEMP;
+	temp[DIM*300+300] = MIN_TEMP;
+	temp[DIM*200+700] = MIN_TEMP;
+	for(int y=800; y<900; y++){
+		for(int x=400; x<500; x++){
+			temp[x+y*DIM] = MIN_TEMP;
+		}
+	}
+
+    HANDLE_ERROR( cudaMemcpy( data.dev_constSrc, temp, bitmap.image_size(), cudaMemcpyHostToDevice ) );
+
+	for(int y=800; y<DIM; y++){
+		for(int x=0; x<200; x++){
+			temp[x+y*DIM] = MAX_TEMP;
+		}
+	}
+
+    HANDLE_ERROR( cudaMemcpy( data.dev_inSrc, temp, bitmap.image_size(), cudaMemcpyHostToDevice ) );
+	free( temp );
+	
+    bitmap.anim_and_exit( (void (*)(void*,int))anim_gpu, (void (*)(void*))anim_exit );
+}
+
+
+void test_heat_animation_tex (const bool isTex2D)
+{
+	DataBlock_heat	data;
+	CPUAnimBitmap bitmap( DIM, DIM, &data );
+	data.bitmap = &bitmap;
+	data.totalTime = 0;
+	data.frames = 0;
+    data.isTex2D = isTex2D;
+	HANDLE_ERROR( cudaEventCreate( &data.start ) );
+	HANDLE_ERROR( cudaEventCreate( &data.stop ) );
+	
+	HANDLE_ERROR( cudaMalloc( (void**)&data.output_bitmap, bitmap.image_size() ) );
+
+    
+    cudaTextureDesc texDesc = {};
+    texDesc.readMode = cudaReadModeElementType;
+    texDesc.filterMode = cudaFilterModePoint;
+    texDesc.normalizedCoords = false;    
+    texDesc.addressMode[0] = cudaAddressModeClamp;
+    texDesc.addressMode[1] = cudaAddressModeClamp;
+
+    size_t pitch;
+    if (isTex2D)
+    {
+        HANDLE_ERROR( cudaMallocPitch( (void**)&data.dev_constSrc, &pitch, DIM * sizeof(float), DIM ) );
+        HANDLE_ERROR( cudaMallocPitch( (void**)&data.dev_inSrc, &pitch, DIM * sizeof(float), DIM ) );
+        HANDLE_ERROR( cudaMallocPitch( (void**)&data.dev_outSrc, &pitch, DIM * sizeof(float), DIM ) );
+
+        cudaResourceDesc resDescConst = {};
+        resDescConst.resType = cudaResourceTypePitch2D;
+        resDescConst.res.pitch2D.devPtr = data.dev_constSrc;
+        resDescConst.res.pitch2D.desc = cudaCreateChannelDesc<float>();
+        resDescConst.res.pitch2D.width = DIM;
+        resDescConst.res.pitch2D.height = DIM;
+        resDescConst.res.pitch2D.pitchInBytes = pitch;
+        cudaCreateTextureObject(&data.texConstSrc, &resDescConst, &texDesc, NULL);
+
+        cudaResourceDesc resDescIn = {};
+        resDescIn.resType = cudaResourceTypePitch2D;
+        resDescIn.res.pitch2D.devPtr = data.dev_inSrc;
+        resDescIn.res.pitch2D.desc = cudaCreateChannelDesc<float>();
+        resDescIn.res.pitch2D.width = DIM;
+        resDescIn.res.pitch2D.height = DIM;
+        resDescIn.res.pitch2D.pitchInBytes = pitch;
+        cudaCreateTextureObject(&data.texIn, &resDescIn, &texDesc, NULL);
+
+        cudaResourceDesc resDescOut = {};
+        resDescOut.resType = cudaResourceTypePitch2D;
+        resDescOut.res.pitch2D.devPtr = data.dev_outSrc;
+        resDescOut.res.pitch2D.desc = cudaCreateChannelDesc<float>();
+        resDescOut.res.pitch2D.width = DIM;
+        resDescOut.res.pitch2D.height = DIM;
+        resDescOut.res.pitch2D.pitchInBytes = pitch;
+        cudaCreateTextureObject(&data.texOut, &resDescOut, &texDesc, NULL);
+    }
+    else
+    {
+        HANDLE_ERROR( cudaMalloc( (void**)&data.dev_inSrc, bitmap.image_size() ) );
+        HANDLE_ERROR( cudaMalloc( (void**)&data.dev_outSrc, bitmap.image_size() ) );
+        HANDLE_ERROR( cudaMalloc( (void**)&data.dev_constSrc, bitmap.image_size() ) );
+
+        cudaResourceDesc resDesc = {};
+        resDesc.resType = cudaResourceTypeLinear;
+        resDesc.res.linear.desc = cudaCreateChannelDesc<float>();
+        resDesc.res.linear.sizeInBytes = bitmap.image_size();
+
+        resDesc.res.linear.devPtr = data.dev_constSrc;
+        cudaCreateTextureObject(&data.texConstSrc, &resDesc, &texDesc, NULL);
+        
+        resDesc.res.linear.devPtr = data.dev_inSrc;
+        cudaCreateTextureObject(&data.texIn, &resDesc, &texDesc, NULL);
+        
+        resDesc.res.linear.devPtr = data.dev_outSrc;
+        cudaCreateTextureObject(&data.texOut, &resDesc, &texDesc, NULL);
+    }
+	
+	float *temp = (float*)malloc( bitmap.image_size() );
+	for(int i=0; i<DIM*DIM; i++){
+		temp[i] = 0;
+		int x = i % DIM;
+		int y = i / DIM;
+		if((x>300) && (x<600) && (y>310) && (y<601))
+			temp[i] = MAX_TEMP;
+	}
+	temp[DIM*100+100] = (MAX_TEMP + MIN_TEMP)/2;
+	temp[DIM*700+100] = MIN_TEMP;
+	temp[DIM*300+300] = MIN_TEMP;
+	temp[DIM*200+700] = MIN_TEMP;
+	for(int y=800; y<900; y++){
+		for(int x=400; x<500; x++){
+			temp[x+y*DIM] = MIN_TEMP;
+		}
+	}
+
+    if (isTex2D)
+    {
+        HANDLE_ERROR( cudaMemcpy2D( data.dev_constSrc, pitch, 
+            temp, DIM * sizeof(float), 
+            DIM * sizeof(float), DIM, 
+            cudaMemcpyHostToDevice ) );
+    }
+    else
+    {
+        HANDLE_ERROR( cudaMemcpy( data.dev_constSrc, temp, bitmap.image_size(), cudaMemcpyHostToDevice ) );
+    }
+
+	for(int y=800; y<DIM; y++){
+		for(int x=0; x<200; x++){
+			temp[x+y*DIM] = MAX_TEMP;
+		}
+	}
+
+    if (isTex2D)
+    {
+        HANDLE_ERROR( cudaMemcpy2D( data.dev_inSrc, pitch, 
+                                    temp, DIM * sizeof(float), 
+                                    DIM * sizeof(float), DIM, 
+                                    cudaMemcpyHostToDevice ) );
+    }
+    else
+    {
+        HANDLE_ERROR( cudaMemcpy( data.dev_inSrc, temp, bitmap.image_size(), cudaMemcpyHostToDevice ) );
+    }
+    
+	free( temp );
+	
+    bitmap.anim_and_exit( (void (*)(void*,int))anim_gpu_tex, (void (*)(void*))anim_exit );
+}
+
+
+
+
+#pragma endregion
+
 int main (int argc, char* argv[])
 {
     //========== laba 3 ================
@@ -675,11 +1060,16 @@ int main (int argc, char* argv[])
     
     
     //========== laba 4 ================
-    testSpheres();
+    //testSpheres();
     //testSpheres_constant();
 
 
     //========== laba 5 ================
+    //test_heat_animation();
+
+    bool isTex2D = true;
+    isTex2D = false;
+    test_heat_animation_tex(isTex2D);
 
     return 0;
 }
